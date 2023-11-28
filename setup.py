0aed181eb635c4b24598cfe467ce1eecaeb19491
Diab Haque <diabhaque@gmail.com>
2023-11-28 19:37:57 +0800

    Installed necessary packages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='japan_re',
-    version='1.0',
-    description='Japan Real Estate Data Science',
-    author='Diab Haque',
-    author_email='diabhaque@gmail.com',
-    packages=['japan_re']
+    name="japan_re",
+    version="1.0",
+    description="Japan Real Estate Data Science",
+    author="Diab Haque",
+    author_email="diabhaque@gmail.com",
+    packages=["japan_re"],
 )
