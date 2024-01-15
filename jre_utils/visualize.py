217df71f8509128a56404fc2481b36b332f73de0
Diab Haque <diabhaque@gmail.com>
2024-01-15 18:17:23 +0800

    Temporal smoothing notebook

--- jre_utils/visualize.py
+++ jre_utils/visualize.py
@@ -4,7 +4,15 @@ import matplotlib.pyplot as plt
 
 
 def plot_time_series(
-    df, column, group_by_columns, granularity_columns, title, visible=None
+    df,
+    column,
+    group_by_columns,
+    granularity_columns,
+    title,
+    visible=None,
+    width=1600,
+    height=800,
+    showlegend=True,
 ):
     plot_df = df.set_index(group_by_columns)
     plot_df = plot_df[column].unstack(list(range(len(granularity_columns))))
@@ -13,5 +21,11 @@ def plot_time_series(
     fig = px.line(plot_df, y=plot_df.columns, title=title)
 
     fig.update_traces(mode="lines+markers", visible=visible)
-    fig.update_layout(autosize=False, width=1600, height=800, hovermode="closest")
+    fig.update_layout(
+        autosize=False,
+        width=width,
+        height=height,
+        hovermode="closest",
+        showlegend=showlegend,
+    )
     fig.show()
